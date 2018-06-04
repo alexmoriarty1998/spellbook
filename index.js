5a9b37adf63400b6545e82cee926459de7f9fa91
Alex Moriarty <almoriar@iu.edu>
2018-06-04 18:03:35 -0400

    With enter key response.

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+/**
+ when using the enter key to submit the form, the code runs,
+ for about half a second and then stops working? The button 
+ works fine though for some reason.
+ */
 const btn = document.getElementById('btn');
 const message = document.getElementById('greeting');
 const name = document.getElementById('name');
 const instruction = document.getElementById('instruction');
 const form = document.getElementById('form');
-btn.addEventListener('click', function(){
-    if(name.value.length < 1 || food.value.length < 1)
+btn.addEventListener('click', submitted);
+name.addEventListener('keypress',function(e){
+    const key = e.keyCode;
+    if(key === 13){
+        submitted
+    }
+});
+
+
+function submitted()
+{
+    if(name.value.length < 1)
     {
         alert('I think you forgot to fill out some of the form, complete it before submitting');
     }
@@ -12,8 +27,10 @@ btn.addEventListener('click', function(){
     {
         instruction.textContent = "Thanks for completing that";
         instruction.classList.add('pressed');
-        message.textContent = "Have a good day " + name.value + ". I hope you get to eat some " +
-        food.value + ".";
+        message.textContent = "Have a good day " + name.value + "!";
         form.classList.add('hide-form');
     }
-});
+}
+
+
+
