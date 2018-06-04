0f019d5976c069a3323aec44d5c951f1f3c84569
Alex Moriarty <almoriar@iu.edu>
2018-06-04 17:14:21 -0400

    Homework One Completed

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const btn = document.querySelector('button');
+const btn = document.getElementById('btn');
+const message = document.getElementById('greeting');
+const name = document.getElementById('name');
+const instruction = document.getElementById('instruction');
+const form = document.getElementById('form');
 btn.addEventListener('click', function(){
-    console.log('hi')
+    if(name.value.length < 1 || food.value.length < 1)
+    {
+        alert('I think you forgot to fill out some of the form, complete it before submitting');
+    }
+    else
+    {
+        instruction.textContent = "Thanks for completing that";
+        instruction.classList.add('pressed');
+        message.textContent = "Have a good day " + name.value + ". I hope you get to eat some " +
+        food.value + ".";
+        form.classList.add('hide-form');
+    }
 });
