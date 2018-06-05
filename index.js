de051ffd0d8280c672131ad28877b2a6245cbed8
Alex Moriarty <almoriar@iu.edu>
2018-06-05 15:09:16 -0400

    allows the program to work without using innerHTML.

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ const changeHeading = function(ev) {
   const form = ev.target
   const spellName = form.spellName.value
   const spellLevel = form.level.value
-  const spellsDiv = document.querySelector('#spells')
-  //const item = document.createElement("li");
-  //const content = document.createTextNode(`${spellName}, level ${spellLevel} required.`);
+  
+
+
+
   if(spellName.length > 0 && spellLevel.length > 0){
-    spellsDiv.innerHTML += `<li>${spellName}, level ${spellLevel} required.</li>`
+    //spellsDiv.innerHTML += `<li>${spellName}, level ${spellLevel} required.</li>`
+    const newItem = document.createElement("LI");
+    const textNode = document.createTextNode(`${spellName}, level ${spellLevel} required.`);
+    newItem.appendChild(textNode);
+    const spellsList = document.querySelector('#spells');
+    spellsList.insertBefore(newItem, spellsList.childNodes[0]);
   } else{
     alert("you've left one of the fields empty. Please fill out both to submit.")
   }
   
-  f.reset()
+  form.reset()
 }
 
 form.addEventListener('submit', changeHeading)
