14a2d4243e0261370d230ec9d4851e460a4ded69
Alex Moriarty <almoriar@iu.edu>
2018-06-06 20:31:17 -0400

    adds elements to array

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-const form = document.querySelector('form')
-
-const renderProperty =  function(name, value)
+const spells = []
+const app = 
 {
-  const el = document.createElement('span')
-  el.textContent = value
-  el.classList.add('name')
-  return el
+  init: function() {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', ev => {
+      this.handleSubmit(ev)
+    })
+  },
+
+  renderProperty: function(name, value) {
+    const el = document.createElement('span')
+    el.textContent = value
+    el.classList.add(name)
+    return el
+  },
+
+  renderItem: function(spell) {
+    // ['name', 'level']
+    properties = Object.keys(spell)
+
+    // collect an array of renderProperty's return values
+    // (an array of <span> elements)
+    const childElements = properties.map(property => {
+      return this.renderProperty(property, spell[property])
+    })
+
+    const item = document.createElement('li')
+    item.classList.add('spell')
+
+    // append each <span> to the <li>
+    childElements.forEach(el => {
+      item.appendChild(el)
+    })
+
+    return item
+  },
+
+  handleSubmit: function(ev) {
+    ev.preventDefault()
+
+    const f = ev.target
+
+    const spell = {
+      name: f.spellName.value,
+      level: f.level.value,
+    }
+
+    spells.push(spell)
+    console.log(spells)
+
+    const item = this.renderItem(spell)
+
+    const list = document.querySelector('#spells')
+    list.appendChild(item)
+
+    f.reset()
+  },
+
+  
+
+  
+
+  
+
+
+
+  
 }
 
-const handleSubmit = function(ev) {
-  ev.preventDefault()
-  // the source of the event we are listening for
-  const f = ev.target
-  // the value submitted into the field asking for the name of the spell 
-  const spellName = f.spellName.value
-  // the value submitted into the field asking for the level of the spell
-  const level = f.level.value
-
-  /** the container of the the spells and levels that will be displayed on the page,
-      values come from form submissions
-   **/
-  const list = document.querySelector('#spells')
-
-  /**
-   creates two separate containers within the list container, allows for easier differentiation,
-   of styling between the name and level fields
-   */
-  const nameSpan = document.createElement('span')
-  nameSpan.textContent = spellName
-  nameSpan.classList.add('spellName')
-  const levelSpan = document.createElement('span')
-  levelSpan.textContent = level
-  levelSpan.classList.add('level')
-  
-  
-
-  // creates a list which holds the values of both spans above, placed within the original ul 
-  const item = document.createElement('li')
-  item.classList.add('spell')
-  item.appendChild(nameSpan)
-  item.appendChild(levelSpan)
-  list.appendChild(item)
-
-  f.reset()
-}
-
-form.addEventListener('submit', handleSubmit)
-
-
-
-
-
-
-
-
-
-
+app.init()
